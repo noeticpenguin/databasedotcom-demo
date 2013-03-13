56f64861d41dbda9099e5ae262a9bceefe477c08
Kevin Poorman <kjp@brightleafsoftware.com>
2013-03-13 08:46:36 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Databasedotcom::Application.config.secret_token = '95345f567301c33da82a09e409d7cc12a86193570d2f3d9b6776e87a1fc6f3c1b8c2c3771ff959cf3a612c2110c7e9f2af64b6028b6387a872fba64a45b688ff'
