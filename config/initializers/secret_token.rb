db47a45870f2a9545081e582d300e7db4f92b465
Kevin Poorman <kjp@brightleafsoftware.com>
2013-03-13 08:56:24 -0400

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Databasedotcom::Application.config.secret_token = 'cc1aa7a0d851ba9f8523ad615ec444c119a9ac78a192938ec2b704496ed6c9f819863cb384e05bc51e5a35aaaf80030745e2e535cc81bfef2426cebe23cad0a5'
+Databasedotcom::Application.config.secret_token = '82c9984bfbdb98bd6ea043af161e0a48e5b52d97a7e03b6159b816d1eb0b4ca236778fc21568c41f543507a48e8ce7c5f301eba3c17ca706c79fbf468336d073'
